80a5d0a5e002f866e570e4b5f2e0d5f9421b9d0d
Maddy091 <60197336+Maddy091@users.noreply.github.com>
2023-05-28 15:13:48 +0530

    first commmit

---
+++ Calendar_Django_API/settings.py
@@ -0,0 +1,82 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'rc_ug30glux%4)fdawd9bp-ojr0h9g50(6(935qhwu-47glk5'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    # Add your app name here
+    'My_Calendar_API',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+ROOT_URLCONF = 'Calendar_Django_API.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'Calendar_Django_API.wsgi.application'
+
+# Database configuration (change as per your database settings)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+# CORS configuration
+CORS_ORIGIN_ALLOW_ALL = True
+
+# Google OAuth2 configuration
+GOOGLE_CLIENT_SECRET_FILE = 'C:/Users/User/Calendar_Django_API/client_secret.json'
+# settings.py
+
+GOOGLE_REDIRECT_URI = 'http://127.0.0.1:8000/rest/v1/calendar/redirect/'
+GOOGLE_REDIRECT_URI = 'http://127.0.0.1:8000/rest/v1/calendar/init/'
+GOOGLE_REDIRECT_URI = 'http://localhost:8000/rest/v1/calendar/redirect/'
+GOOGLE_REDIRECT_URI = 'http://localhost:8000/rest/v1/calendar/init/'
+
+# Rest of your settings...
+
+# Static and media file configuration...
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
---
+++ Calendar_Django_API/views.py
@@ -0,0 +1,49 @@
+from distutils.command import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.views import View
+
+class GoogleCalendarInitView(View):
+    def get(self, request):
+        flow = InstalledAppFlow.from_client_secrets_file(
+            settings.GOOGLE_CLIENT_SECRET_FILE,
+            scopes=['https://www.googleapis.com/auth/calendar.readonly']
+        )
+        authorization_url, state = flow.authorization_url(
+            access_type='offline',
+            include_granted_scopes='true'
+        )
+        request.session['google_auth_state'] = state
+        return HttpResponseRedirect(authorization_url)
+
+class GoogleCalendarRedirectView(View):
+    def get(self, request):
+        state = request.session.get('google_auth_state')
+        if state is None or state != request.GET.get('state'):
+            return HttpResponseRedirect('/')
+
+        flow = InstalledAppFlow.from_client_secrets_file(
+            settings.GOOGLE_CLIENT_SECRET_FILE,
+            scopes=['https://www.googleapis.com/auth/calendar.readonly'],
+            state=state
+        )
+        flow.fetch_token(
+            authorization_response=request.build_absolute_uri(),
+            redirect_uri=request.build_absolute_uri('/rest/v1/calendar/redirect/')
+        )
+        credentials = flow.credentials
+
+        # Store the credentials in your database or session for future use
+        request.session['google_auth_credentials'] = credentials.to_json()
+
+        # Get the list of events from the user's calendar
+        service = build('calendar', 'v3', credentials=credentials)
+        events_result = service.events().list(calendarId='primary', maxResults=10).execute()
+        events = events_result.get('items', [])
+
+        # Process the events as per your requirement
+
+        return HttpResponseRedirect('/')
